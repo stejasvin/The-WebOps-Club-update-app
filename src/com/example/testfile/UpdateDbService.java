10ec8dd1736d988a78608118a47d836555f2d0d6
stejasvin <stejasvin@gmail.com>
2013-08-25 10:56:20 +0530

    v1.1 notification added

--- src/com/example/testfile/UpdateDbService.java
+++ src/com/example/testfile/UpdateDbService.java
@@ -1,6 +1,9 @@
 package com.example.testfile;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -41,8 +44,6 @@ public class UpdateDbService extends IntentService {
 
     /**
      * Creates an IntentService.  Invoked by your subclass's constructor.
-     *
-     *
      */
     public UpdateDbService() {
         super("UpdateDbService");
@@ -65,12 +66,12 @@ public class UpdateDbService extends IntentService {
 
         SharedPreferences mPrefs = getSharedPreferences(
                 "webops.SHARED_PREF", 0);
-
-
         String lastServerId = mPrefs.getString("com.dhilcare.app.LAST_ID", "0");
-        if(updateDb(lastServerId)){
+        if (updateDb(lastServerId)) {
             sendBroadcast(new Intent(UpdatesListActivity.REFRESH_ACTION));
-        };
+            generateNotification(UpdateDbService.this,"Updates from webops!");
+        }
+        ;
 
     }
 
@@ -98,14 +99,14 @@ public class UpdateDbService extends IntentService {
             is = httpEntity.getContent();
 
             String resp = isParse(is);
-            Log.i(TAG,resp);
+            Log.i(TAG, resp);
             JSONArray jsonArray = new JSONArray(resp);
 
             addMessages(jsonArray);
 
             return true;
 
-        } catch(JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
@@ -122,17 +123,22 @@ public class UpdateDbService extends IntentService {
     private void addMessages(JSONArray jsonArray) {
 
         UpdatesDbHandler updatesDbHandler = new UpdatesDbHandler(UpdateDbService.this);
+        SharedPreferences preferences = getSharedPreferences("webops.SHARED_PREF", 0);
+        SharedPreferences.Editor mEditor = preferences.edit();
 
-        for(int i=0;i<jsonArray.length();i++){
-            try{
-            Update update = new Update();
-            update.setServerId(jsonArray.getJSONObject(i).getString("id"));
-            update.setTitle(jsonArray.getJSONObject(i).getString("title"));
-            update.setMessage(jsonArray.getJSONObject(i).getString("message"));
-            update.setLocalTime(getCurrentTime());
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                Update update = new Update();
+                update.setServerId(jsonArray.getJSONObject(i).getString("id"));
+                update.setTitle(jsonArray.getJSONObject(i).getString("title"));
+                update.setMessage(jsonArray.getJSONObject(i).getString("message"));
+                update.setLocalTime(getCurrentTime());
 
-            updatesDbHandler.addUpdate(update);
-            }catch (JSONException e){
+                updatesDbHandler.addUpdate(update);
+
+                mEditor.putString("com.dhilcare.app.LAST_ID", update.getServerId()).commit();
+
+            } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
@@ -170,5 +176,38 @@ public class UpdateDbService extends IntentService {
         Calendar c = Calendar.getInstance();
         String currentTime = (c.getTime()).toString();
         return currentTime;
+    }
+
+    public static void generateNotification(Context context, String message) {
+//        Log.i(TAG, "Generating notification + count: " + count);
+
+        int icon = R.drawable.ic_launcher;
+
+        long when = System.currentTimeMillis();
+
+        NotificationManager notificationManager = (NotificationManager) context
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+
+        Notification notification = new Notification(icon, message, when);
+
+        String title = context.getString(R.string.app_name);
+
+        Intent notificationIntent = null;
+
+        notificationIntent = new Intent(context, UpdatesListActivity.class);
+
+        notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent intent = PendingIntent.getActivity(context, 0,
+                notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        notification.setLatestEventInfo(context, title, message, intent);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        // Play default notification sound
+        notification.defaults |= Notification.DEFAULT_SOUND;
+
+        // Vibrate if vibrate is enabled
+        notification.defaults |= Notification.DEFAULT_VIBRATE;
+        notificationManager.notify(0, notification);
     }
 }
